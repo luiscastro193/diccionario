a5fe7478f922aba1bd42c434ca4f0cde0a6686f9
Luis Castro Martín <luiscastro193@gmail.com>
2024-01-11 22:13:43 +0100

    Fix typo

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ let references = {
 	}
 };
 
-const pdfjsLibtPromise = import("https://cdn.jsdelivr.net/npm/pdfjs-dist/build/pdf.min.mjs").then(module => {
+const pdfjsLibPromise = import("https://cdn.jsdelivr.net/npm/pdfjs-dist/build/pdf.min.mjs").then(module => {
 	const pdfjsLib = module;
 	pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.mjs`;
 	return pdfjsLib;
